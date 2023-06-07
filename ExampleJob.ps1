3201599adfe1f652094491a3058d9cc189829dbd
Aditya Hoode <v-ahoode@microsoft.com>
2023-06-07 15:04:05 +0530

    split job for batch and stream

--- ExampleJob.ps1
+++ ExampleJob.ps1
@@ -81,7 +81,7 @@ $HEADERS = @{
 }
 # Set the request body
 $BODY = @"
-    { "lifetime_seconds": 1200, "comment": "Script Test" }
+    { "lifetime_seconds": 1200, "comment": "Job Automation Script" }
 "@
     
 try {
@@ -106,91 +106,131 @@ catch {
     }
 }
 
-try {
-    $pipelineCreateUrl = "https://$WorkspaceUrl/api/2.0/pipelines"
+$HEADERS = @{
+    "Authorization" = "Bearer $DB_PAT"
+    "Content-Type"  = "application/json"
+}
 
-    $HEADERS = @{
-        "Authorization" = "Bearer $DB_PAT"
-        "Content-Type"  = "application/json"
-    }
+# Create the pipeline for the batch processing of the retail org dataset
+Write-Host "[INFO] Create the pipeline for the batch processing of the retail org dataset"
 
-    $pipelineDefinitionJSON = @"
-    {
-        "name": "batch_processing_pipeline_api",
-        "clusters": [
-            {
-                "label": "default",
-                "num_workers": 0
+$pipelineCreateUrl = "https://$WorkspaceUrl/api/2.0/pipelines"
+
+$pipelineDefinitionJSON = @"
+{
+    "name": "retail_org_batch_dlt(API)",
+    "edition": "ADVANCED",
+    "target": "retail_org_batch_api",
+    "clusters": [
+        {
+            "label": "default",
+            "num_workers": 1
+        }
+    ],
+    "development": true,
+    "continuous": false,
+    "channel": "CURRENT",
+    "photon": false,
+    "libraries": [
+        {
+            "notebook": {
+                "path": "/Shared/Example/bronze-layer-notebook"
             }
-        ],
-        "development": true,
-        "continuous": false,
-        "channel": "CURRENT",
-        "photon": false,
-        "libraries": [
-            {
-                "notebook": {
-                    "path": "/Shared/Example/bronze-layer-notebook"
-                }
-            },
-            {
-                "notebook": {
-                    "path": "/Shared/Example/silver-layer-notebook"
-                }
-            },
-            {
-                "notebook": {
-                    "path": "/Shared/Example/gold-layer-notebook"
-                }
+        },
+        {
+            "notebook": {
+                "path": "/Shared/Example/silver-layer-notebook"
             }
-        ],
-        "edition": "ADVANCED",
-        "target": "batch_example_api"
-    }
+        },
+        {
+            "notebook": {
+                "path": "/Shared/Example/gold-layer-notebook"
+            }
+        }
+    ]
+}
 "@
 
-    $pipelineId = (Invoke-RestMethod -Method POST -Uri $pipelineCreateUrl -Headers $HEADERS -Body $pipelineDefinitionJSON).pipeline_id
+try {
+    $batchPipelineId = (Invoke-RestMethod -Method POST -Uri $pipelineCreateUrl -Headers $HEADERS -Body $pipelineDefinitionJSON).pipeline_id
+    Write-Host "[SUCCESS] Batch pipeline created successfully with Pipeline ID: $batchPipelineId"
 }
 catch {
-    Write-Host "Error while calling the Databricks API for creating pipeline"
+    Write-Host "[ERROR] Error while calling the Databricks API for creating the batch pipeline"
     $errorMessage = $_.Exception.Message
     Write-Host "Error message: $errorMessage" 
 }
 
-if ($null -ne $pipelineId) {
-    Write-Host $pipelineId
-    try {
-        $jobCreateUrl = "https://$WorkspaceUrl/api/2.1/jobs/create"
-    
-        $HEADERS = @{
-            "Authorization" = "Bearer $DB_PAT"
-            "Content-Type"  = "application/json"
-        }
-    
-        $jobDefinitionJSON = @"
+# Create jobs and run for the retail org dataset
+Write-Host '[INFO] Create jobs and run them for the retail org dataset'
+
+$jobCreateUrl = "https://$WorkspaceUrl/api/2.1/jobs/create"
+
+$jobRunUrl = "https://$WorkspaceUrl/api/2.1/jobs/run-now"
+
+# Create and run the batch job
+
+$batchJobDefinition = @"
         {
-            "name": "example_job_API",
+            "name": "retail_org_batch(API)",
             "max_concurrent_runs": 1,
             "tasks": [
                 {
-                    "task_key": "batch_processing_pipeline",
+                    "task_key": "batch_processing_dlt",
                     "pipeline_task": {
-                        "pipeline_id": "$pipelineId",
-                        "full_refresh": false
+                        "pipeline_id": "$batchPipelineId"
                     }
-                },
+                }
+            ],
+            "format": "MULTI_TASK"
+        }
+"@
+
+if ($null -ne $batchPipelineId) {
+    try {
+        $batchJobId = (Invoke-RestMethod -Method POST -Uri $jobCreateUrl -Headers $HEADERS -Body $batchJobDefinition).job_id
+        $batchJobCreated = $true
+        Write-Host "[SUCCESS] Job successfully created for batch processing with Job ID: $batchJobId"
+    }
+    catch {
+        $batchJobCreated = $false
+        Write-Host "[ERROR] Error while calling the Databricks API for creating job for batch processing"
+        $errorMessage = $_.Exception.Message
+        Write-Host "Error message: $errorMessage" 
+    }
+}
+
+if ($batchJobCreated) {
+    $batchRunDefinition = @"
+        {
+            "job_id": $batchJobId
+        }
+"@
+    try {
+        $batchRunId = (Invoke-RestMethod -Method POST -Uri $jobRunUrl -Headers $HEADERS -Body $batchRunDefinition).run_id
+        Write-Host "[SUCCESS] Running the job for batch processing with Run ID: $batchRunId"
+    }
+    catch {
+        Write-Host "[ERROR] Error while calling the Databricks API for running the batch processing job"
+        $errorMessage = $_.Exception.Message
+        Write-Host "Error message: $errorMessage" 
+    }
+}
+
+# Create and run the stream job
+
+$streamJobDefinition = @"
+        {
+            "name": "retail_org_stream(API)",
+            "max_concurrent_runs": 1,
+            "tasks": [
                 {
                     "task_key": "publish_events",
-                    "depends_on": [
-                        {
-                            "task_key": "batch_processing_pipeline"
-                        }
-                    ],
                     "notebook_task": {
                         "notebook_path": "/Shared/Example/publish_events-eventhub",
                         "source": "WORKSPACE"
                     },
-                    "job_cluster_key": "Job_cluster_API"
+                    "job_cluster_key": "Job_cluster"
                 },
                 {
                     "task_key": "stream_processing",
@@ -203,7 +243,7 @@ if ($null -ne $pipelineId) {
                         "notebook_path": "/Shared/Example/bronze_silver_gold_stream",
                         "source": "WORKSPACE"
                     },
-                    "job_cluster_key": "Job_cluster_API",
+                    "job_cluster_key": "Job_cluster",
                     "libraries": [
                         {
                             "maven": {
@@ -215,7 +255,7 @@ if ($null -ne $pipelineId) {
             ],
             "job_clusters": [
                 {
-                    "job_cluster_key": "Job_cluster_API",
+                    "job_cluster_key": "Job_cluster",
                     "new_cluster": {
                         "cluster_name": "",
                         "spark_version": "12.2.x-scala2.12",
@@ -223,6 +263,11 @@ if ($null -ne $pipelineId) {
                             "spark.databricks.delta.preview.enabled": "true",
                             "spark.master": "local[*, 4]",
                             "spark.databricks.cluster.profile": "singleNode"
+                        },
+                        "azure_attributes": {
+                            "first_on_demand": 1,
+                            "availability": "ON_DEMAND_AZURE",
+                            "spot_bid_max_price": -1
                         },
                         "node_type_id": "Standard_DS3_v2",
                         "custom_tags": {
@@ -242,45 +287,31 @@ if ($null -ne $pipelineId) {
         }
 "@
     
-        $jobId = (Invoke-RestMethod -Method POST -Uri $jobCreateUrl -Headers $HEADERS -Body $jobDefinitionJSON).job_id
-        $jobCreated = $true
-    }
-    catch {
-        $jobCreated = $false
-        Write-Host "Error while calling the Databricks API for creating Job"
-        $errorMessage = $_.Exception.Message
-        Write-Host "Error message: $errorMessage" 
-    }
-    
+try {
+    $streamJobId = (Invoke-RestMethod -Method POST -Uri $jobCreateUrl -Headers $HEADERS -Body $streamJobDefinition).job_id
+    $streamJobCreated = $true
+    Write-Host "[SUCCESS] Job successfully created for stream processing with Job ID: $streamJobId"
+}
+catch {
+    $streamJobCreated = $false
+    Write-Host "[ERROR] Error while calling the Databricks API for creating job for stream processing"
+    $errorMessage = $_.Exception.Message
+    Write-Host "Error message: $errorMessage" 
 }
 
-if ($jobCreated) {
-    Write-Host $jobId
-    try {
-        $jobRunUrl = "https://$WorkspaceUrl/api/2.1/jobs/run-now"
 
-        $HEADERS = @{
-            "Authorization" = "Bearer $DB_PAT"
-            "Content-Type"  = "application/json"
-        }
-            
-        $runDefinitionJSON = @"
-        {
-            "idempotency_token": "101",
-            "job_id": $jobId
-        }
+if ($streamJobCreated) {
+    $streamRunDefinition = @"
+    {
+        "job_id": $streamJobId
+    }
 "@
-
-        $runId = (Invoke-RestMethod -Method POST -Uri $jobRunUrl -Headers $HEADERS -Body $runDefinitionJSON).run_id
-        
-        if ($null -ne $runId) {
-            Write-Host "Job running with Run ID: $runId"
-        } else {
-            Write-Host "Job not running"
-        }
+    try {
+        $streamRunId = (Invoke-RestMethod -Method POST -Uri $jobRunUrl -Headers $HEADERS -Body $streamRunDefinition).run_id
+        Write-Host "[SUCCESS] Running the job for stream processing with Run ID: $streamRunId"
     }
     catch {
-        Write-Host "Error while calling the Databricks API for running job"
+        Write-Host "[ERROR] Error while calling the Databricks API for running the stream processing job"
         $errorMessage = $_.Exception.Message
         Write-Host "Error message: $errorMessage" 
     }
